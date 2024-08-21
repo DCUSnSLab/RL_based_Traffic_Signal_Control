8125a3e93066d09452456262cf36f2d1f530515a
KwonHyeokGyu <ko9493@cu.ac.kr>
2024-08-21 23:36:04 +0900

    적응형 신호제어 추가

--- runactuatedocc.py
+++ runactuatedocc.py
@@ -2,7 +2,7 @@ import traci
 from RunSimulation import RunSimulation
 
 
-class RunActuated(RunSimulation):
+class RunActuatedOCC(RunSimulation):
     def __init__(self, config, name):
         super().__init__(config, name)
         self.cycle_time = 200
@@ -58,18 +58,16 @@ class RunActuated(RunSimulation):
         green_times = {}
         surplus_rates = {}
         waiting_times = {}
-        capacity_percentage = {"0": 25, "1": 20, "2": 28, "3": 27}
+        vehicle_by_bound={"0": 107, "1": 88, "2": 126, "3": 119}
 
-        # 바운드의 가중치 계산
-        total_weight = sum(
-            [section.traffic_queue / capacity_percentage[section_id] for section_id, section in sections.items()])
+        total_percentage = sum([section.traffic_queue / vehicle_by_bound[section_id] for section_id, section in sections.items()])
 
         for section_id, section in sections.items():
             vehicle_count = section.traffic_queue
-            capacity_percent = capacity_percentage[section_id]
+            occupancy_rate = section.traffic_queue / vehicle_by_bound[section_id]
 
             # 가중치 기반으로 신호 시간 계산
-            green_time = self.calculate_green_time(vehicle_count, capacity_percent, total_weight, total_green_time)
+            green_time = self.calculate_green_time_by_percentage(occupancy_rate, total_percentage, total_green_time)
             green_times[section_id] = round(green_time)
 
             # 계산된 green_time을 사용하여 surplus rate 및 waiting time 계산
@@ -99,10 +97,9 @@ class RunActuated(RunSimulation):
         print("0 : Sb, 1 : Nb, 2 : Eb, 3 : Wb]")
         print(f"Green times: {green_times}, Surplus rates: {surplus_rates}, Waiting times: {waiting_times}")
 
-    def calculate_green_time(self, queue_length, capacity_percentage, total_weight, total_green_time):
-        # 가중치 기반 신호 시간 계산
-        weight = queue_length / capacity_percentage
-        return (weight / total_weight) * total_green_time
+
+    def calculate_green_time_by_percentage(self, occupancy_rate, total_percentage, total_green_time):
+        return(occupancy_rate/total_percentage)*total_green_time
 
     def calculate_surplus_rate(self, vehicle_count, service_rate):
         return vehicle_count - service_rate
